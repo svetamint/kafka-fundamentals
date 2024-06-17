984dcbc2c5acf643786c9e824bab6566ba8956ea
s.lebiadzko <svetamint@gmail.com>
2024-06-17 11:39:10 +0200

    add UK as default locale for tests running

--- kafka-producer/src/test/java/com/godeltech/config/KafkaTestContainersConfiguration.java
+++ kafka-producer/src/test/java/com/godeltech/config/KafkaTestContainersConfiguration.java
@@ -3,6 +3,9 @@ package com.godeltech.config;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.Locale;
 
 @Configuration
 public class KafkaTestContainersConfiguration {
@@ -10,6 +13,13 @@ public class KafkaTestContainersConfiguration {
     @Bean
     public TestRestTemplate restTemplate() {
         return new TestRestTemplate();
+    }
+
+    @Bean
+    @Primary
+    public Locale defaultLocale() {
+        Locale.setDefault(Locale.ENGLISH);
+        return Locale.ENGLISH;
     }
 
 
